0eb4c4f89d07ebd04758b7914440e583a44eb8fe
Vincent Chan <vincent@vincentcchan.com>
2014-03-11 19:20:47 -0700

    recursion exercises

--- experiment.js
+++ experiment.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
 
 { head: 1, tail: { head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } } }
@@ -8,21 +10,165 @@ Converts to:
 
 */
 
-function reverse (l) {
- if (l === null) { return null; }
- else {
- return append(reverse(l.tail), 
- { head: l.head,
- tail: null});
- }
+function append (l1,l2) {
+  if (l1 === null) {
+    return l2;
+  }
+  else {
+    return {
+      head: l1.head,
+      tail: append(l1.tail,l2)
+    };
+  }
 }
 
-function listReverse(list, iterator){
- if(list == null){ return null }
- if(list. !== 0){  }
- else return append(reverse(list.tail, list.head-1),  {
-   head: list.head,
-   tail: null
+
+//recursive quadric
+
+function reverse (l) {
+  if (l === null) {
+    return null;
   }
- }
+  else {
+    return append( reverse(l.tail) ,  { head: l.head, tail: null });
+  }
 }
+
+
+//iterative linear
+
+function linReverse(list){
+
+  if(list === null){ return null; }
+
+  function r(l, lt){
+    console.log(l)
+    console.log(lt)
+    console.log(l.tail)
+    console.log(lt.tail)
+    if(lt.tail === null){
+      return { head: l.head, tail: null}
+    }
+
+    return append( r(lt.head, l.tail), {head: lt.head, tail: l.tail} )
+    // return append( r(l.head, lt.tail) , { head: l.head, tail: l.tail }) ;
+  }
+
+  if(list.tail !== null){
+    return r(list, list.tail);
+  }
+}
+
+/* some figuring out */
+
+/* STEP 1
+append(   reverse({ head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }),
+          {head: 1, tail: null};  )
+
+append()'s
+return
+------------------------
+      =>  {
+            head: reverse( { head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }).head,
+            tail: append( reverse({ head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }).tail.tail, {head: 1, tail: null} )
+          }
+
+
+
+
+--STEP 2
+
+
+reverse({ head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } })
+//turn into
+
+append(   reverse( { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }),
+          {head: 2, tail: null};  )
+
+
+next append()'s head's
+return
+------------------------
+      => {
+            head: reverse( { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }).head,
+            tail: append( reverse( { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }).tail.tail, {head: 2, tail: null} )
+          }
+
+
+--STEP 3
+
+next append()'s head's
+return
+------------------------
+      => {
+            head: reverse( { head: 4, tail: { head: 5, tail: null } } ).head,
+            tail: append( reverse( { head: 4, tail: { head: 5, tail: null } } ).tail.tail, {head: 3, tail: null} )
+          }
+
+
+--STEP 4
+
+next append()'s head's
+return
+------------------------
+      =>  {
+            head: reverse( { head: 5, tail: null } ).head,
+            tail: append( reverse( { head: 5, tail: null } ).tail ).tail, {head: 4, tail: null} )
+          }
+
+--STEP 5
+
+next append()'s head's
+return
+------------------------
+      =>  {
+            head: reverse(null).head,
+            tail: append( reverse(null).tail ).tail, {head: 5, tail: null} )
+          }
+
+      =>  {
+            head: null.head,
+            tail: append( null.tail.tail, {head: 5, tail: null} )
+          }
+
+      =>  {
+            head: 5,
+            tail: null
+          }
+
+
+STEP 2
+==========
+l.head = 2;
+l.tail = { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } }
+l = {head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }
+
+return append( reverse({ head: 2, tail: { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } } }).tail.tail, {head: 1, tail: null} )
+=> { head: 5, tail: { head: 4, tail: null } }
+
+
+STEP 3
+==========
+l.head = 3;
+l.tail = { head: 4, tail: { head: 5, tail: null } }
+l = { head: 3, tail: { head: 4, tail: { head: 5, tail: null } } }
+
+STEP 4
+==========
+l.head = 4;
+l.tail = { head: 5, tail: null }
+l = { head: 4, tail: { head: 5, tail: null } }
+
+STEP 5
+==========
+l.head = 5;
+l.tail = null
+l = { head: 5, tail: null }
+
+
+
+*/
+
+
+
+
